4d660de5e7fca08a5f225225e117afc50ee79980
Sayantika-ai <sayantikapvt@gmail.com>
2026-07-03 21:35:35 +0530

    docs: Add industry-standard header comments to sum.c

--- sum.c
+++ sum.c
@@ -1,5 +1,18 @@
-# include <stdio.h>
+/*******************************************************************************
+ * @file        sum.c
+ * @brief       Calculates the summation metrics for basic user input arrays.
+ * @author      Sayantika Das (CSE-AI&ML Specialized Track)
+ * @institution Chandigarh University (CUIMS: 25CSH-107)
+ * @date        July 03, 2026
+ * 
+ * @details     Developed as part of the Semester 1 GPA Protection Track.
+ *              Implements standard input streaming and memory tracking.
+ *              Optimized for clean compilation under GCC/MinGW configurations.
+ *******************************************************************************/
 
+
+
+#include <stdio.h>
 int main() {
     int a,b;
     printf("enter a");
